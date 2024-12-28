223590422be38a132a5d9f0d1f8c5f73aa74dfd1
Ahmed Tarek <ahmed.tarek1244@gmail.com>
2024-12-28 23:20:11 +0200

    finish mail remender cron job

--- src/basket/basket.module.ts
+++ src/basket/basket.module.ts
@@ -7,12 +7,18 @@ import { AuthModule } from 'src/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGaurd } from 'src/auth/guards/auth.guard';
+import { MailModule } from 'src/mail/mail.module';
+import { CouponsModule } from 'src/coupons/coupons.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Basket]),
-  AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([Basket]),
+    AuthModule,
+    MailModule,
+    CouponsModule,
+  ],
   controllers: [BasketController],
   providers: [BasketService],
-  exports:[BasketService]
+  exports: [BasketService],
 })
 export class BasketModule {}
--- src/basket/basket.service.ts
+++ src/basket/basket.service.ts
@@ -8,11 +8,18 @@ import { Basket } from './basket.entity';
 import { LessThan, Repository } from 'typeorm';
 import { Users } from 'src/users/users.entity';
 import { Cron } from '@nestjs/schedule';
+import { MailService } from 'src/mail/mail.service';
+import { CouponsService } from 'src/coupons/coupons.service';
+import { UUID } from 'typeorm/driver/mongodb/bson.typings';
+import { CouponType } from 'src/coupons/coupon-type.enum';
+import { Coupon } from 'src/coupons/coupon.entity';
 
 @Injectable()
 export class BasketService {
   constructor(
     @InjectRepository(Basket) private basketRepo: Repository<Basket>,
+    private readonly mailService: MailService,
+    private readonly couponsService: CouponsService,
   ) {}
 
   private async createBasket(user: Users): Promise<Basket> {
@@ -50,35 +57,70 @@ export class BasketService {
     await this.getBasketById(id);
     await this.basketRepo.update(
       { id },
-      { updated_at: new Date(), reminder_sent: false },
+      { updated_at: (new Date()).toISOString(), reminder_sent: false },
     );
   }
 
-  // @Cron('*/10 * * * * *')
-  // async basketRemender() {
-  //   try {
-  //     const now = new Date();
-  //     const hours = String(now.getHours()).padStart(2, '0');
-  //     const minutes = String(now.getMinutes()).padStart(2, '0');
-  //     const seconds = String(now.getSeconds()).padStart(2, '0');
+  @Cron('*/10 * * * * *')
+  async basketRemender() {
+    try {
+      // const now = new Date();
+      // const hours = String(now.getHours()).padStart(2, '0');
+      // const minutes = String(now.getMinutes()).padStart(2, '0');
+      // const seconds = String(now.getSeconds()).padStart(2, '0');
 
-  //     console.log(
-  //       `Hey, I am logging time now is: ${hours}:${minutes}:${seconds}`,
-  //     );
+      // console.log(
+      //   `Hey, I am logging time now is: ${hours}:${minutes}:${seconds}`,
+      // );
 
-  //     const oneHourAgo = new Date();
-  //     oneHourAgo.setHours(oneHourAgo.getHours() - 1);
+      const twoHoursAgo = new Date();
+      twoHoursAgo.setHours(twoHoursAgo.getHours() - 2);
 
-  //     const baskets = await this.basketRepo.find({
-  //       where: {
-  //         reminder_sent: false,
-  //         updated_at: LessThan(oneHourAgo), // updated_at is older than one hour
-  //       },
-  //     });
-  //     console.log(baskets);
+      const afterOneWeek = new Date();
+      afterOneWeek.setHours(afterOneWeek.getHours() + 7 * 24);
+
+      const basketss = await this.basketRepo.find();
       
-  //   } catch (err) {
-  //     throw new InternalServerErrorException(err);
-  //   }
-  // }
+      const baskets = await this.basketRepo.find({
+        where: {
+          reminder_sent: false,
+          updated_at: LessThan(twoHoursAgo),
+        },
+        relations: ['user'],
+      });
+
+      await Promise.all(
+        baskets.map(async (basket) => {
+          const coupon: Coupon = await this.couponsService.createCoupon({
+            amount: 20,
+            expiryDate: afterOneWeek,
+            name: this.generateRandomString(),
+            type: CouponType.precentage,
+            usageLimit: 1,
+          });
+          // console.log(`coupon: ${coupon}`);
+          // await this.mailService.sendExclusiveCouponToUser(
+          //   coupon.name,
+          //   basket.user.email,
+          // );
+
+          basket.reminder_sent = true;
+
+          await this.basketRepo.save(basket);
+        }),
+      );
+    } catch (err) {
+      throw new InternalServerErrorException(err);
+    }
+  }
+
+  private generateRandomString(length: number = 8): string {
+    const chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  }
 }
